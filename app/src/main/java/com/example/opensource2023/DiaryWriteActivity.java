2a651e8ba887f37db14c74317394c831784bb17e
GimHaLim <halim7568@gmail.com>
2023-11-23 16:14:27 +0900

    feat : 화면 전환

--- app/src/main/java/com/example/opensource2023/DiaryWriteActivity.java
+++ app/src/main/java/com/example/opensource2023/DiaryWriteActivity.java
@@ -2,6 +2,7 @@ package com.example.opensource2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,20 +19,33 @@ public class DiaryWriteActivity extends AppCompatActivity {
 
     ImageButton diaryCheckButton;
     ImageButton diaryClearButton;
+    TextView monthView;
 
 
     EditText diaryWrite;
+    String month;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_write);
 
-        diaryCheckButton.findViewById(R.id.DiaryCheckButton);
-        diaryClearButton.findViewById(R.id.DiaryClearButton);
-        diaryWrite.findViewById(R.id.DiaryWrite);
+//        diaryCheckButton.findViewById(R.id.DiaryCheckButton);
+//        diaryClearButton.findViewById(R.id.DiaryClearButton);
+//        diaryWrite.findViewById(R.id.DiaryWrite);
+        monthView = (TextView) findViewById(R.id.Month);
+        Intent intent = getIntent(); /*데이터 수신*/
+
+        month = intent.getExtras().getString("month");
+        int m = Integer.parseInt(month);
+        monthView.setText(getMonthName(m));
 
 
+    }
+
+    private String getMonthName(int month) {
+        String[] monthNames = new String[] {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        return monthNames[month - 1];
     }
 
 }
